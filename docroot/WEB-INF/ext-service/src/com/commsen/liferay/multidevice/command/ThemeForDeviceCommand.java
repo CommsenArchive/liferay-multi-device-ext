dcb9fde5c2dbbaa92e5d8c1c2853cf78e8cc474d
Milen Dyankov <milendyankov@gmail.com>
2011-02-25 07:16:32 +0100

    Changed license text

--- docroot/WEB-INF/ext-service/src/com/commsen/liferay/multidevice/command/ThemeForDeviceCommand.java
+++ docroot/WEB-INF/ext-service/src/com/commsen/liferay/multidevice/command/ThemeForDeviceCommand.java
@@ -1,20 +1,19 @@
 /**
- *	This file is part of multi-device portal extension for Liferay.
+ * Copyright (c) COMMSEN International. All rights reserved.
  *	
- * Multi-device portal extension for Liferay is free software: you can redistribute it and/or modify 
+ * This library is free software: you can redistribute it and/or modify 
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  * 
- * Multi-device portal extension for Liferay is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with multi-device portal extension for Liferay.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ * along with this library.  If not, see http://www.gnu.org/licenses/lgpl.html.
  */
-
 package com.commsen.liferay.multidevice.command;
 
 import com.commsen.liferay.multidevice.Device;
